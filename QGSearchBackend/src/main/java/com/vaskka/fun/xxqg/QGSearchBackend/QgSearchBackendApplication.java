34d5a288a9e3a3548d289ef0d8f09197e5d628d9
Vaskka <1139851358@qq.com>
2019-08-03 22:00:38 +0800

    init backend

---
+++ QGSearchBackend/src/main/java/com/vaskka/fun/xxqg/QGSearchBackend/QgSearchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vaskka.fun.xxqg.QGSearchBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QgSearchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QgSearchBackendApplication.class, args);
+	}
+
+}
